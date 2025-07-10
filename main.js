a9114ab290f52df04afbb27b535611ef0f90b54e
setokk <tesmtracks@gmail.com>
2025-07-11 00:34:50 +0300

    Changed styles

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
+'use strict';
+
+/* ---------------- */
+/* Type Definitions */
+/* ---------------- */
+const PageSectionEnum = {
+    HOME: 'home',
+    PORTFOLIO: 'portfolio',
+    ABOUT_ME: 'about'
+}
+
+const CategoryEnum = {
+    PHOTOGRAPHY: 0,
+    GRAPHIC_DESIGN: 1
+}
+
+class ImageInfoDto {
+    id;             // string
+    category;       // CategoryEnum
+    title;          // string
+    description;    // string
+    imgSrc;         // string
+
+    constructor(id, category, title, description, imgSrc) {
+        this.id = id;
+        this.category = category;
+        this.title = title;
+        this.description = description;
+        this.imgSrc = imgSrc;
+    }
+}
+
+/* ---- */
+/* Main */
+/* ---- */
 const home_btn = document.getElementById('home-btn');
 const portfolio_btn = document.getElementById('portfolio-btn');
 const about_btn = document.getElementById('about-btn');
@@ -6,27 +41,28 @@ const home_elements = Array.from(document.getElementsByClassName('home'));
 const portfolio_elements = Array.from(document.getElementsByClassName('portfolio'));
 const about_elements = Array.from(document.getElementsByClassName('about'));
 
-console.log(home_elements)
-
 home_btn.addEventListener('click', () => {
-    show_selected_elements_for('home')
+    show_selected_elements_for(PageSectionEnum.HOME)
 }, false);
 portfolio_btn.addEventListener('click', () => {
-    show_selected_elements_for('portfolio')
+    show_selected_elements_for(PageSectionEnum.PORTFOLIO)
 }, false);
 about_btn.addEventListener('click', () => {
-    show_selected_elements_for('about')
+    show_selected_elements_for(PageSectionEnum.ABOUT_ME)
 }, false);
 
 
+/* -------------------- */
+/* Function Definitions */
+/* -------------------- */
 function show_selected_elements_for(name) {
     home_elements.forEach((element) => {
-        element.style.display = (name === 'home') ? 'flex' : 'none';
+        element.style.display = (name === PageSectionEnum.HOME) ? 'flex' : 'none';
     });
     portfolio_elements.forEach((element) => {
-        element.style.display = (name === 'portfolio') ? 'flex' : 'none';
+        element.style.display = (name === PageSectionEnum.PORTFOLIO) ? 'flex' : 'none';
     });
     about_elements.forEach((element) => {
-        element.style.display = (name === 'about') ? 'flex' : 'none';
+        element.style.display = (name === PageSectionEnum.ABOUT_ME) ? 'flex' : 'none';
     });
 }
