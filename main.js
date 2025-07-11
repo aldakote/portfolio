763b9f395ab070c6274fded8b10b3cc903a0d2d1
setokk <tesmtracks@gmail.com>
2025-07-11 18:43:58 +0300

    Added test payload data

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const PageSectionEnum = {
 }
 
 const CategoryEnum = {
-    PHOTOGRAPHY: 0,
-    GRAPHIC_DESIGN: 1
+    PHOTOGRAPHY: 'photography',
+    GRAPHIC_DESIGN: 'graphic_design'
 }
 
 class ImageInfoDto {
@@ -19,14 +19,28 @@ class ImageInfoDto {
     category;       // CategoryEnum
     title;          // string
     description;    // string
+    extension;      // string
     imgSrc;         // string
 
-    constructor(id, category, title, description, imgSrc) {
+    constructor(id, category, title, description, extension, imgSrc) {
         this.id = id;
         this.category = category;
         this.title = title;
         this.description = description;
+        this.extension = extension;
         this.imgSrc = imgSrc;
+    }
+}
+
+class DB {
+    async getImagesForCategory(category) {
+        const response = await fetch(`./db/${category}_data.json`);
+        if (!response.ok) {
+            throw new Error('Error trying to connect with database');
+        }
+
+        const json = await response.json();
+        return json.map(res => new ImageInfoDto(res.id, category, res.title, res.description, res.extension, `./images/${res.id}.${res.extension}`));
     }
 }
 
@@ -36,21 +50,30 @@ class ImageInfoDto {
 const home_btn = document.getElementById('home-btn');
 const portfolio_btn = document.getElementById('portfolio-btn');
 const about_btn = document.getElementById('about-btn');
+const logo_span = document.getElementById('logo-span');
 
 const home_elements = Array.from(document.getElementsByClassName('home'));
 const portfolio_elements = Array.from(document.getElementsByClassName('portfolio'));
 const about_elements = Array.from(document.getElementsByClassName('about'));
 
+show_selected_elements_for(PageSectionEnum.HOME);
+
+const db = new DB();
+load_all_images(db);
+
+/* Event Listeners */
 home_btn.addEventListener('click', () => {
-    show_selected_elements_for(PageSectionEnum.HOME)
+    show_selected_elements_for(PageSectionEnum.HOME);
 }, false);
 portfolio_btn.addEventListener('click', () => {
-    show_selected_elements_for(PageSectionEnum.PORTFOLIO)
+    show_selected_elements_for(PageSectionEnum.PORTFOLIO);
 }, false);
 about_btn.addEventListener('click', () => {
-    show_selected_elements_for(PageSectionEnum.ABOUT_ME)
+    show_selected_elements_for(PageSectionEnum.ABOUT_ME);
 }, false);
-
+logo_span.addEventListener('click', () => {
+    show_selected_elements_for(PageSectionEnum.HOME);
+}, false);
 
 /* -------------------- */
 /* Function Definitions */
@@ -65,4 +88,11 @@ function show_selected_elements_for(name) {
     about_elements.forEach((element) => {
         element.style.display = (name === PageSectionEnum.ABOUT_ME) ? 'flex' : 'none';
     });
+}
+
+async function load_all_images(db) {
+    const photographyImageList = await db.getImagesForCategory(CategoryEnum.PHOTOGRAPHY);
+    const graphicDesignImageList = await db.getImagesForCategory(CategoryEnum.GRAPHIC_DESIGN);
+    console.log(photographyImageList);
+    console.log(graphicDesignImageList);
 }
